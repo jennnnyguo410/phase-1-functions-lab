7b30be92aed204afeab79886dcc5b924dbb57131
Hao Guo <jennnnyguo@gmail.com>
2023-05-08 17:15:00 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42)
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare = 0
+    const feets = distanceTravelledInFeet(start, destination)
+    if (feets <= 400) {
+        return fare
+    } else if (feets > 400 && feets <= 2000) {
+        return fare = (feets * 2 - 800) / 100
+    } else if (feets > 2000 && feets <= 2500) {
+        return fare = 25
+    } else if (feets > 2500) {
+        return fare = 'cannot travel that far'
+    }
+}
+
